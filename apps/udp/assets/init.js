e89849996560b29b807e66cc02f94a96382c7513
Alexandru Grigoroi <alexandru@grigoroi.com>
2014-03-02 18:40:08 +0000

    Moved back

--- apps/udp/assets/init.js
+++ apps/udp/assets/init.js
@@ -15,7 +15,7 @@ var config = {
   }
 };
 
-var socket = io.connect('http://project.grigoroi.com:1000');
+var socket = io.connect('http://project.grigoroi.com:1345');
 
 function onConnect() {
 
--- console.js
+++ console.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(1000);
+var io = require('socket.io').listen(1345);
 
 nodes = {};
 
@@ -12,7 +12,7 @@ setInterval(function() {
   console.log(nodes);
 }, 5000);
 
-var io2 = require('socket.io').listen(1001);
+var io2 = require('socket.io').listen(1346);
 io2.sockets.on('connection', function(socket) {
   setInterval(function() {
     socket.emit("nodes", nodes);
